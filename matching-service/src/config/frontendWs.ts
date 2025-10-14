41ec6ecb3d7be01105609a0be83a22d1d9904496
Trisha Patodia <trishapatodia@Trishas-MacBook-Air-4.local>
2025-10-14 14:46:38 +0800

    Resolved frontendWS.ts data mismatch issue

--- matching-service/src/config/frontendWs.ts
+++ matching-service/src/config/frontendWs.ts
@@ -1,8 +1,8 @@
-// frontendSocket.js
-const { Server } = require("socket.io");
-const { registerFrontendClient } = require("./wsClient");
+// frontendSocket.ts
+import { Server } from "socket.io";
+import { registerFrontendClient } from "./wsClient";
 
-function setupFrontendSocket(server) {
+function setupFrontendSocket(server: any) {
     const io = new Server(server, {
         cors: {
             origin: "*", // change this to your frontend URL in production
@@ -11,11 +11,11 @@ function setupFrontendSocket(server) {
 
     io.on("connection", (socket) => {
         console.log("[SOCKET.IO SERVER] Frontend connected:", socket.id);
-        registerFrontendClient(socket);
+        registerFrontendClient(socket as any);
         console.log(`[SOCKET.IO SERVER] Total clients: ${io.sockets.sockets.size}`);
     });
 
     return io;
 }
 
-module.exports = { setupFrontendSocket };
+export { setupFrontendSocket };
