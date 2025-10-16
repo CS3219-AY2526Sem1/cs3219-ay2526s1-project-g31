2bbf78a953a2d8db6e74884739b8e3353b6a6e16
Billy026 <92001719+Billy026@users.noreply.github.com>
2025-10-16 22:27:06 +0800

    Add UI for header and given question

--- ui/src/app/collaboration/page.tsx
+++ ui/src/app/collaboration/page.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { useEffect, useState } from "react";
+import { Question } from "shared";
 import { io, Socket } from "socket.io-client";
+
 
 let socket: Socket;
 
 export default function CollaborationPage() {
+    const [user, setUser] = useState("User");
+    const [partner, setPartner] = useState("Partner");
+    const [question, setQuestion] = useState<Question>();
+
     useEffect(() => {
         const socket: Socket = io("http://localhost:3004");
 
@@ -14,8 +20,11 @@ export default function CollaborationPage() {
             socket.emit("joinDummyRoom");
         });
 
-        socket.on("roomData", (room) => {
+        socket.on("receiveRoomData", (room) => {
             console.log("[UI] Dummy room data received:", room);
+            setUser(room.users[0].displayName);
+            setPartner(room.users[1].displayName);
+            setQuestion(room.question);
         });
 
         socket.on("disconnect", () => {
@@ -27,16 +36,36 @@ export default function CollaborationPage() {
     
     return (
         <div className="relative min-h-screen flex flex-col">
-            <h1 className="text-white text-5xl font-bold z-10">
-                1
-            </h1>
+            <div className="bg-blue-500 flex items-center pt-1 pb-1 pl-3 pr-3">
+                <h2 className="text-white text-3xl font-bold">PeerPrep</h2>
+                
+                <div className="flex-1"></div>
 
-            <button
-                onClick={ () => console.log("clicked") }
-                className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
-            >
-                Click me
-            </button>
+                <p className="pr-1">{user}</p>
+                <p className="pl-1">{partner}</p>
+            </div>
+
+            <div className="flex flex-1">
+                <div className="flex flex-1 flex-col">
+                    <div className="flex-1 bg-blue-900 p-4 overflow-y-auto">
+                        <h1 className="text-white text-center font-bold underline text-4xl mb-2">
+                            {question?.title}
+                        </h1>
+                        
+                        <p className="text-white">
+                            {question?.description}
+                        </p>
+                    </div>
+                    <div className="flex-1 bg-black p-2">
+                        <h1>Hi</h1>
+                    </div>
+                </div>
+                <div className="flex-1 bg-orange-400 p-5">
+                    <div className="w-full h-full bg-black">
+                        <h1>Hi</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
