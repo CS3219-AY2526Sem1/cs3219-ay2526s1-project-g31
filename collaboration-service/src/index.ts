b68282b8011a98d88bd132594ff448f500899364
matochichap <ruijia.chan@gmail.com>
2025-10-23 19:45:36 +0800

    dockerise collaboration service build

--- collaboration-service/src/index.ts
+++ collaboration-service/src/index.ts
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 import path from "path";
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
 import http from "http";
 import app from "./server";
 import { initializeSocketServer } from "./socketServer";
 
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const PORT = process.env.COLLABORATION_SERVICE_PORT || 3004;
 const server = http.createServer(app);
