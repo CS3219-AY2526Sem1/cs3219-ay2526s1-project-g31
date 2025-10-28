f171e29c7dd9001d048cc3c995b1572da3a1c972
matochichap <ruijia.chan@gmail.com>
2025-10-28 13:17:04 +0800

    set all question get requests to user and admin level, rest of the requests to admin level

--- api-gateway/src/routes/question.ts
+++ api-gateway/src/routes/question.ts
@@ -6,12 +6,26 @@ import { UserRole } from "shared";
 const questionRouter = Router();
 const QUESTION_SERVICE_URL = `http://${process.env.BASE_URL}:${process.env.QUESTION_SERVICE_PORT}`;
 
-questionRouter.use("/api/question",
-    verifyAccessToken,
-    authorizedRoles([UserRole.USER, UserRole.ADMIN]),
-    attachUserFromJwt,
-    proxyMiddleware(QUESTION_SERVICE_URL, "/api/question"),
-);
+// NOTE: 
+// generically set all GET requests under /api/question/* to USER + ADMIN
+// and all other methods(POST, PUT, DELETE) to ADMIN only
+// this can be modified later if more fine grained control is needed
+
+questionRouter.route("/api/question")
+    // GET: USER + ADMIN
+    .get(
+        verifyAccessToken,
+        authorizedRoles([UserRole.USER, UserRole.ADMIN]),
+        attachUserFromJwt,
+        proxyMiddleware(QUESTION_SERVICE_URL, "/api/question")
+    )
+    // POST, PUT, DELETE: ADMIN only
+    .all(
+        verifyAccessToken,
+        authorizedRoles([UserRole.ADMIN]),
+        attachUserFromJwt,
+        proxyMiddleware(QUESTION_SERVICE_URL, "/api/question")
+    );
 
 
 export { questionRouter };
