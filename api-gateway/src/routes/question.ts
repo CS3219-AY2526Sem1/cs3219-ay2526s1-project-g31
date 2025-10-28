1e11f7864f3fbc4fa0dc247a7d23c596641a725a
matochichap <ruijia.chan@gmail.com>
2025-10-28 13:33:19 +0800

    add rbac for question routes

--- api-gateway/src/routes/question.ts
+++ api-gateway/src/routes/question.ts
@@ -11,21 +11,27 @@ const QUESTION_SERVICE_URL = `http://${process.env.BASE_URL}:${process.env.QUEST
 // and all other methods(POST, PUT, DELETE) to ADMIN only
 // this can be modified later if more fine grained control is needed
 
-questionRouter.route("/api/question")
+questionRouter.route("/api/question/")
     // GET: USER + ADMIN
     .get(
         verifyAccessToken,
         authorizedRoles([UserRole.USER, UserRole.ADMIN]),
         attachUserFromJwt,
-        proxyMiddleware(QUESTION_SERVICE_URL, "/api/question")
+        proxyMiddleware(QUESTION_SERVICE_URL)
     )
-    // POST, PUT, DELETE: ADMIN only
-    .all(
+    // POST: ADMIN only
+    .post(
         verifyAccessToken,
         authorizedRoles([UserRole.ADMIN]),
         attachUserFromJwt,
         proxyMiddleware(QUESTION_SERVICE_URL, "/api/question")
     );
 
+questionRouter.use("/api/question/random",
+    verifyAccessToken,
+    authorizedRoles([UserRole.USER, UserRole.ADMIN]),
+    attachUserFromJwt,
+    proxyMiddleware(QUESTION_SERVICE_URL, "/api/question/random")
+);
 
 export { questionRouter };
