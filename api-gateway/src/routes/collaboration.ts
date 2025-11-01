3b8e76dc84ed194ec8d2c14a8d4d74349b4d499d
matochichap <ruijia.chan@gmail.com>
2025-11-01 12:09:44 +0800

    change api gateway routing to be more specific

--- api-gateway/src/routes/collaboration.ts
+++ api-gateway/src/routes/collaboration.ts
@@ -6,22 +6,7 @@ import { UserRole } from "shared";
 const collaborationRouter = Router();
 const COLLABORATION_SERVICE_URL = process.env.COLLABORATION_SERVICE_BASE_URL!;
 
-collaborationRouter.use("/api/collaboration",
-    verifyAccessToken,
-    authorizedRoles([UserRole.USER, UserRole.ADMIN]),
-    attachUserFromJwt,
-    proxyMiddleware(COLLABORATION_SERVICE_URL, "/api/collaboration"),
-);
-
-collaborationRouter.use("/api/room",
-    verifyAccessToken,
-    authorizedRoles([UserRole.USER, UserRole.ADMIN]),
-    attachUserFromJwt,
-    proxyMiddleware(COLLABORATION_SERVICE_URL, "/api/room"),
-);
-
-collaborationRouter.use("/socket/collaboration",
-    proxyMiddleware(COLLABORATION_SERVICE_URL, "/socket/collaboration", true)
-);
+// add your endpoints here, including the socket endpoint
+// see other route files for examples
 
 export { collaborationRouter };
--- api-gateway/src/routes/match.ts
+++ api-gateway/src/routes/match.ts
@@ -6,13 +6,15 @@ import { UserRole } from "shared";
 const matchRouter = Router();
 const MATCHING_SERVICE_URL = process.env.MATCHING_SERVICE_BASE_URL!;
 
-matchRouter.use("/api/match",
+matchRouter.post(
+    "/api/match/start",
     verifyAccessToken,
     authorizedRoles([UserRole.USER, UserRole.ADMIN]),
     attachUserFromJwt,
-    proxyMiddleware(MATCHING_SERVICE_URL, "/api/match"),
+    proxyMiddleware(MATCHING_SERVICE_URL),
 );
 
+// socket.io verifies access token, no need to re-verify here
 matchRouter.use("/socket/matching",
     proxyMiddleware(MATCHING_SERVICE_URL, "/socket/matching", true)
 );
