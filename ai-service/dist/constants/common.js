e6e596f0ae2b794343fb209e8c0982756eb12598
matochichap <ruijia.chan@gmail.com>
2025-11-08 13:51:16 +0800

    remove dist, node_modules folders and .env files

--- ai-service/dist/constants/common.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UI_BASE_URL = void 0;
-exports.UI_BASE_URL = `http://${process.env.BASE_URL}:${process.env.UI_PORT}`;
